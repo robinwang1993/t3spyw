707ea23fffb18631d5a2df4e21755f1e2c004e46
robin <robinwang1993@gmail.com>
2018-09-14 20:45:03 +0800

    Initial commit

---
+++ src/main/java/com/imooc/netty/ch3/Server.java
@@ -0,0 +1,44 @@
+package com.imooc.netty.ch3;
+
+import com.imooc.netty.ch6.AuthHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.AttributeKey;
+
+/**
+ * @author
+ */
+public final class Server {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)//设置服务端的socketchannel
+                    .childOption(ChannelOption.TCP_NODELAY, true)//设置一些TCP的基本属性
+                    .childAttr(AttributeKey.newInstance("childAttr"), "childAttrValue")//创建客户端连接时，绑定基本属性
+                    .handler(new ServerHandler())//服务端启动过程中的相关逻辑
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new AuthHandler());
+                            //..
+
+                        }
+                    });
+
+            ChannelFuture f = b.bind(8888).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
